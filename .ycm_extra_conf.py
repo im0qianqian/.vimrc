76d360fca6db0aa15e7b382e87a6d8d7693c16ae
im0qianqian <qian1335661317@qq.com>
2019-02-04 14:35:26 +0800

    for linux

--- .ycm_extra_conf.py
+++ .ycm_extra_conf.py
@@ -33,72 +33,87 @@ import platform
 import os.path as p
 import subprocess
 import ycm_core
+import sys
 
-DIR_OF_THIS_SCRIPT = p.abspath( p.dirname( __file__ ) )
-DIR_OF_THIRD_PARTY = p.join( DIR_OF_THIS_SCRIPT, 'third_party' )
-SOURCE_EXTENSIONS = [ '.cpp', '.cxx', '.cc', '.c', '.m', '.mm' ]
+DIR_OF_THIS_SCRIPT = p.abspath(p.dirname(__file__))
+DIR_OF_THIRD_PARTY = p.join(DIR_OF_THIS_SCRIPT, 'third_party')
+SOURCE_EXTENSIONS = ['.cpp', '.cxx', '.cc', '.c', '.m', '.mm']
 
 # These are the compilation flags that will be used in case there's no
 # compilation database set (by default, one is not set).
 # CHANGE THIS LIST OF FLAGS. YES, THIS IS THE DROID YOU HAVE BEEN LOOKING FOR.
 flags = [
-'-std=c++14',
-'-g',
-'-DLOCAL_IM0QIANQIAN',
-'-pipe'
-'-Wall',
-'-Wextra',
-'-Werror',
-'-Wno-long-long',
-'-Wno-variadic-macros',
-'-fexceptions',
-'-DNDEBUG',
-# You 100% do NOT need -DUSE_CLANG_COMPLETER and/or -DYCM_EXPORT in your flags;
-# only the YCM source code needs it.
-'-DUSE_CLANG_COMPLETER',
-'-DYCM_EXPORT=',
-# THIS IS IMPORTANT! Without the '-x' flag, Clang won't know which language to
-# use when compiling headers. So it will guess. Badly. So C++ headers will be
-# compiled as C headers. You don't want that so ALWAYS specify the '-x' flag.
-# For a C project, you would set this to 'c' instead of 'c++'.
-'-x',
-'c++',
-# '-isystem',
-# 'cpp/pybind11',
-# '-isystem',
-# 'cpp/whereami',
-# '-isystem',
-# 'cpp/BoostParts',
-# '-isystem',
-# get_python_inc(),
-# '-isystem',
-# 'cpp/llvm/include',
-# '-isystem',
-# 'cpp/llvm/tools/clang/include',
-# '-I',
-# 'cpp/ycm',
-# '-I',
-# 'cpp/ycm/ClangCompleter',
-# '-isystem',
-# 'cpp/ycm/tests/gmock/gtest',
-# '-isystem',
-# 'cpp/ycm/tests/gmock/gtest/include',
-# '-isystem',
-# 'cpp/ycm/tests/gmock',
-# '-isystem',
-# 'cpp/ycm/tests/gmock/include',
-# '-isystem',
-# 'cpp/ycm/benchmarks/benchmark/include',
-'-I',
-'C:/Program Files (x86)/CodeBlocks/mingw64/lib/gcc/x86_64-w64-mingw32/8.1.0/include/c++/x86_64-w64-mingw32',
-'-I',
-'C:/Program Files (x86)/CodeBlocks/mingw64/lib/gcc/x86_64-w64-mingw32/8.1.0/include/c++/tr1',
-'-I',
-'C:/Program Files (x86)/CodeBlocks/mingw64/lib/gcc/x86_64-w64-mingw32/8.1.0/include/c++',
-'-I',
-'C:/Program Files (x86)/CodeBlocks/mingw64/lib/gcc/x86_64-w64-mingw32/8.1.0/include',
-'-I',
-'C:/Program Files (x86)/CodeBlocks/mingw64/x86_64-w64-mingw32/include',
+    '-std=c++14',
+    '-g',
+    '-DLOCAL_IM0QIANQIAN',
+    '-pipe'
+    '-Wall',
+    '-Wextra',
+    '-Werror',
+    '-Wno-long-long',
+    '-Wno-variadic-macros',
+    '-fexceptions',
+    '-DNDEBUG',
+    # You 100% do NOT need -DUSE_CLANG_COMPLETER and/or -DYCM_EXPORT in your flags;
+    # only the YCM source code needs it.
+    '-DUSE_CLANG_COMPLETER',
+    '-DYCM_EXPORT=',
+    # THIS IS IMPORTANT! Without the '-x' flag, Clang won't know which language to
+    # use when compiling headers. So it will guess. Badly. So C++ headers will be
+    # compiled as C headers. You don't want that so ALWAYS specify the '-x' flag.
+    # For a C project, you would set this to 'c' instead of 'c++'.
+    '-x',
+    'c++',
+    # '-isystem',
+    # 'cpp/pybind11',
+    # '-isystem',
+    # 'cpp/whereami',
+    # '-isystem',
+    # 'cpp/BoostParts',
+    # '-isystem',
+    # get_python_inc(),
+    # '-isystem',
+    # 'cpp/llvm/include',
+    # '-isystem',
+    # 'cpp/llvm/tools/clang/include',
+    # '-I',
+    # 'cpp/ycm',
+    # '-I',
+    # 'cpp/ycm/ClangCompleter',
+    # '-isystem',
+    # 'cpp/ycm/tests/gmock/gtest',
+    # '-isystem',
+    # 'cpp/ycm/tests/gmock/gtest/include',
+    # '-isystem',
+    # 'cpp/ycm/tests/gmock',
+    # '-isystem',
+    # 'cpp/ycm/tests/gmock/include',
+    # '-isystem',
+    # 'cpp/ycm/benchmarks/benchmark/include',
+    '-I',
+    'C:/Program Files (x86)/CodeBlocks/mingw64/lib/gcc/x86_64-w64-mingw32/8.1.0/include/c++/x86_64-w64-mingw32',
+    '-I',
+    'C:/Program Files (x86)/CodeBlocks/mingw64/lib/gcc/x86_64-w64-mingw32/8.1.0/include/c++/tr1',
+    '-I',
+    'C:/Program Files (x86)/CodeBlocks/mingw64/lib/gcc/x86_64-w64-mingw32/8.1.0/include/c++',
+    '-I',
+    'C:/Program Files (x86)/CodeBlocks/mingw64/lib/gcc/x86_64-w64-mingw32/8.1.0/include',
+    '-I',
+    'C:/Program Files (x86)/CodeBlocks/mingw64/x86_64-w64-mingw32/include',
+    '-I',
+    "/usr/include/c++/8.2.1/x86_64-pc-linux-gnu",
+    '-I',
+    "/usr/include/c++/8.2.1/backward",
+    '-I',
+    "/usr/include/c++/8.2.1",
+    '-I',
+    "/usr/lib/gcc/x86_64-pc-linux-gnu/8.2.1/include-fixed",
+    '-I',
+    "/usr/lib/gcc/x86_64-pc-linux-gnu/8.2.1/include",
+    '-I',
+    "/usr/local/include",
+    '-I',
+    "/usr/include",
 ]
 
 # Set this to the absolute path to the folder (NOT the file!) containing the
@@ -113,122 +128,123 @@ flags = [
 # 'flags' list of compilation flags. Notice that YCM itself uses that approach.
 compilation_database_folder = ''
 
-if p.exists( compilation_database_folder ):
-  database = ycm_core.CompilationDatabase( compilation_database_folder )
+if p.exists(compilation_database_folder):
+    database = ycm_core.CompilationDatabase(compilation_database_folder)
 else:
-  database = None
+    database = None
 
 
-def IsHeaderFile( filename ):
-  extension = p.splitext( filename )[ 1 ]
-  return extension in [ '.h', '.hxx', '.hpp', '.hh' ]
+def IsHeaderFile(filename):
+    extension = p.splitext(filename)[1]
+    return extension in ['.h', '.hxx', '.hpp', '.hh']
 
 
-def FindCorrespondingSourceFile( filename ):
-  if IsHeaderFile( filename ):
-    basename = p.splitext( filename )[ 0 ]
-    for extension in SOURCE_EXTENSIONS:
-      replacement_file = basename + extension
-      if p.exists( replacement_file ):
-        return replacement_file
-  return filename
+def FindCorrespondingSourceFile(filename):
+    if IsHeaderFile(filename):
+        basename = p.splitext(filename)[0]
+        for extension in SOURCE_EXTENSIONS:
+            replacement_file = basename + extension
+            if p.exists(replacement_file):
+                return replacement_file
+    return filename
 
 
 def PathToPythonUsedDuringBuild():
-  try:
-    filepath = p.join( DIR_OF_THIS_SCRIPT, 'PYTHON_USED_DURING_BUILDING' )
-    with open( filepath ) as f:
-      return f.read().strip()
-  # We need to check for IOError for Python 2 and OSError for Python 3.
-  except ( IOError, OSError ):
-    return None
-
-
-def Settings( **kwargs ):
-  language = kwargs[ 'language' ]
-
-  if language == 'cfamily':
-    # If the file is a header, try to find the corresponding source file and
-    # retrieve its flags from the compilation database if using one. This is
-    # necessary since compilation databases don't have entries for header files.
-    # In addition, use this source file as the translation unit. This makes it
-    # possible to jump from a declaration in the header file to its definition
-    # in the corresponding source file.
-    filename = FindCorrespondingSourceFile( kwargs[ 'filename' ] )
-
-    if not database:
-      return {
-        'flags': flags,
-        'include_paths_relative_to_dir': DIR_OF_THIS_SCRIPT,
-        'override_filename': filename
-      }
-
-    compilation_info = database.GetCompilationInfoForFile( filename )
-    if not compilation_info.compiler_flags_:
-      return {}
-
-    # Bear in mind that compilation_info.compiler_flags_ does NOT return a
-    # python list, but a "list-like" StringVec object.
-    final_flags = list( compilation_info.compiler_flags_ )
-
-    # NOTE: This is just for YouCompleteMe; it's highly likely that your project
-    # does NOT need to remove the stdlib flag. DO NOT USE THIS IN YOUR
-    # ycm_extra_conf IF YOU'RE NOT 100% SURE YOU NEED IT.
     try:
-      final_flags.remove( '-stdlib=libc++' )
-    except ValueError:
-      pass
-
-    return {
-      'flags': final_flags,
-      'include_paths_relative_to_dir': compilation_info.compiler_working_dir_,
-      'override_filename': filename
-    }
-
-  if language == 'python':
-    client_data = kwargs[ 'client_data' ]
-    return {
-      # 'interpreter_path': client_data[ 'g:ycm_python_interpreter_path' ],
-      'sys_path': client_data[ 'g:ycm_python_sys_path' ],
-      'interpreter_path': PathToPythonUsedDuringBuild()
-    }
-
-  return {}
+        filepath = p.join(DIR_OF_THIS_SCRIPT, 'PYTHON_USED_DURING_BUILDING')
+        with open(filepath) as f:
+            return f.read().strip()
+    # We need to check for IOError for Python 2 and OSError for Python 3.
+    except (IOError, OSError):
+        return None
 
 
-def GetStandardLibraryIndexInSysPath( sys_path ):
-  for index, path in enumerate( sys_path ):
-    if p.isfile( p.join( path, 'os.py' ) ):
-      return index
-  raise RuntimeError( 'Could not find standard library path in Python path.' )
+def Settings(**kwargs):
+    language = kwargs['language']
+
+    if language == 'cfamily':
+        # If the file is a header, try to find the corresponding source file and
+        # retrieve its flags from the compilation database if using one. This is
+        # necessary since compilation databases don't have entries for header files.
+        # In addition, use this source file as the translation unit. This makes it
+        # possible to jump from a declaration in the header file to its definition
+        # in the corresponding source file.
+        filename = FindCorrespondingSourceFile(kwargs['filename'])
+
+        if not database:
+            return {
+                'flags': flags,
+                'include_paths_relative_to_dir': DIR_OF_THIS_SCRIPT,
+                'override_filename': filename
+            }
+
+        compilation_info = database.GetCompilationInfoForFile(filename)
+        if not compilation_info.compiler_flags_:
+            return {}
+
+        # Bear in mind that compilation_info.compiler_flags_ does NOT return a
+        # python list, but a "list-like" StringVec object.
+        final_flags = list(compilation_info.compiler_flags_)
+
+        # NOTE: This is just for YouCompleteMe; it's highly likely that your project
+        # does NOT need to remove the stdlib flag. DO NOT USE THIS IN YOUR
+        # ycm_extra_conf IF YOU'RE NOT 100% SURE YOU NEED IT.
+        try:
+            final_flags.remove('-stdlib=libc++')
+        except ValueError:
+            pass
+
+        return {
+            'flags':
+            final_flags,
+            'include_paths_relative_to_dir':
+            compilation_info.compiler_working_dir_,
+            'override_filename':
+            filename
+        }
+
+    if language == 'python':
+        client_data = kwargs['client_data']
+        return {
+            # 'interpreter_path': client_data[ 'g:ycm_python_interpreter_path' ],
+            # 'sys_path': client_data['g:ycm_python_sys_path']
+            'sys_path': sys.path,
+            'interpreter_path': PathToPythonUsedDuringBuild()
+        }
+
+    return {}
 
 
-def PythonSysPath( **kwargs ):
-  sys_path = kwargs[ 'sys_path' ]
+def GetStandardLibraryIndexInSysPath(sys_path):
+    for index, path in enumerate(sys_path):
+        if p.isfile(p.join(path, 'os.py')):
+            return index
+    raise RuntimeError('Could not find standard library path in Python path.')
 
-  interpreter_path = kwargs[ 'interpreter_path' ]
-  major_version = subprocess.check_output( [
-    interpreter_path, '-c', 'import sys; print( sys.version_info[ 0 ] )' ]
-  ).rstrip().decode( 'utf8' )
-  sys_path.insert( GetStandardLibraryIndexInSysPath( sys_path ) + 1,
-                   p.join( DIR_OF_THIRD_PARTY, 'python-future', 'src' ) )
-  sys_path[ 0:0 ] = [ p.join( DIR_OF_THIS_SCRIPT ),
-                      p.join( DIR_OF_THIRD_PARTY, 'bottle' ),
-                      p.join( DIR_OF_THIRD_PARTY, 'cregex',
-                              'regex_{}'.format( major_version ) ),
-                      p.join( DIR_OF_THIRD_PARTY, 'frozendict' ),
-                      p.join( DIR_OF_THIRD_PARTY, 'jedi_deps', 'jedi' ),
-                      p.join( DIR_OF_THIRD_PARTY, 'jedi_deps', 'parso' ),
-                      p.join( DIR_OF_THIRD_PARTY, 'requests_deps', 'requests' ),
-                      p.join( DIR_OF_THIRD_PARTY, 'requests_deps',
-                                                  'urllib3',
-                                                  'src' ),
-                      p.join( DIR_OF_THIRD_PARTY, 'requests_deps',
-                                                  'chardet' ),
-                      p.join( DIR_OF_THIRD_PARTY, 'requests_deps',
-                                                  'certifi' ),
-                      p.join( DIR_OF_THIRD_PARTY, 'requests_deps',
-                                                  'idna' ),
-                      p.join( DIR_OF_THIRD_PARTY, 'waitress' ) ]
 
-  return sys_path
+def PythonSysPath(**kwargs):
+    sys_path = kwargs['sys_path']
+
+    interpreter_path = kwargs['interpreter_path']
+    major_version = subprocess.check_output(
+        [interpreter_path, '-c',
+         'import sys; print( sys.version_info[ 0 ] )']).rstrip().decode('utf8')
+    sys_path.insert(
+        GetStandardLibraryIndexInSysPath(sys_path) + 1,
+        p.join(DIR_OF_THIRD_PARTY, 'python-future', 'src'))
+    sys_path[0:0] = [
+        p.join(DIR_OF_THIS_SCRIPT),
+        p.join(DIR_OF_THIRD_PARTY, 'bottle'),
+        p.join(DIR_OF_THIRD_PARTY, 'cregex', 'regex_{}'.format(major_version)),
+        p.join(DIR_OF_THIRD_PARTY, 'frozendict'),
+        p.join(DIR_OF_THIRD_PARTY, 'jedi_deps', 'jedi'),
+        p.join(DIR_OF_THIRD_PARTY, 'jedi_deps', 'parso'),
+        p.join(DIR_OF_THIRD_PARTY, 'requests_deps', 'requests'),
+        p.join(DIR_OF_THIRD_PARTY, 'requests_deps', 'urllib3', 'src'),
+        p.join(DIR_OF_THIRD_PARTY, 'requests_deps', 'chardet'),
+        p.join(DIR_OF_THIRD_PARTY, 'requests_deps', 'certifi'),
+        p.join(DIR_OF_THIRD_PARTY, 'requests_deps', 'idna'),
+        p.join(DIR_OF_THIRD_PARTY, 'waitress')
+    ]
+
+    return sys_path
